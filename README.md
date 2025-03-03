c7015e11b0660bd430d7efabc481aecdf9291c14
tulolo287 <iv1055@yandex.ru>
2025-03-04 00:04:58 +0300

    add index page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/tulolo287/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/tulolo287/python-project-83/actions)
+### Link to web application
+https://python-project-83-tf17.onrender.com
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
@@ -10,4 +10,4 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.get("/")
 def index():
-    return "Hello, world"
+    return render_template('/pages/index.html')
